3fbdb5b42076540f41af37660e87099f1bd6956c
Zhyldyz Oruzbaeva <zhyldyzoruzbaeva@MacBook-Air-Zhyldyz.local>
2022-09-30 11:17:47 +0600

    Homework

---
+++ TASK1.py
@@ -0,0 +1,18 @@
+Fullname = 'Oruzbaeva Zhyldyz'
+Age = 26
+Gender = 'Female'
+Work_address = 'BSC'
+Study_address = 'Lightcode'
+Direction = 'Python'
+Did_you_study_programming_before = 'No'
+Desired_salary = '2000$'
+
+print ('ФИО:', Fullname)
+print ('Возраст:', Age)
+print ('Пол:', Gender)
+print ('Место работы:', Work_address)
+print ('Место учебы:', Study_address)
+print ('Направление:', Direction)
+print ('Изучали ли програмирование ранее:', Did_you_study_programming_before)
+print ('Желаемая зарплата:', Desired_salary)
+
---
+++ TASK3.py
@@ -0,0 +1,19 @@
+
+
+Country = ' the USA or the UK'
+Salary = ' 10000$'
+Employmenttype = 'Online and Part-time'
+Workingplace = ' Anywhere'
+Workingdays = ' Monday-Thursday'
+Workinghours = ' Flexible working hours'
+WhatelseIwant = ' Healthcare insurance; Paid vacation; Vacation twice a year; Motivations; Business trip'
+
+
+
+print ('Страна:', Country) 
+print ( 'Зарплата:', Salary)
+print ('Тип занятости:', Employmenttype)
+print ('Место работы:', Workingplace)
+print ('Количество рабочих дней:', Workingdays)
+print ('Количество рабочих часов:', Workinghours)
+print ('Что я еще хочу:', WhatelseIwant)
